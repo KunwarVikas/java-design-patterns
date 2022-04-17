c6c841336a9709aad9089579c34527ecf0a6a76e
Kunwar <kunwar.nitw@gmail.com>
2022-04-17 12:59:56 +0530

    Facade Design Pattern Implementation

---
+++ src/main/com/kunwar/designpatterns/structural/facade/IMobileShop.java
@@ -0,0 +1,16 @@
+package main.com.kunwar.designpatterns.structural.facade;
+
+/**
+ * @author Kunwar
+ */
+public interface IMobileShop {
+    /**
+     * Mobile Model Number
+     */
+    public void getMobileModelNumber();
+
+    /**
+     * Mobile Price
+     */
+    public void getMobilePrice();
+}  
---
+++ src/main/com/kunwar/designpatterns/structural/facade/Samsung.java
@@ -0,0 +1,16 @@
+package main.com.kunwar.designpatterns.structural.facade;
+
+/**
+ * @author Kunwar
+ */
+public class Samsung implements IMobileShop {
+    @Override
+    public void getMobileModelNumber() {
+        System.out.println("The model is: Galaxy 11" );
+    }
+
+    @Override
+    public void getMobilePrice() {
+        System.out.println("The price is: 85000INR ");
+    }
+}  
